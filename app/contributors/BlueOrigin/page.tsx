e4abffe56f668a604476115009e1d14fc896f508
kamillamamatova <kamillamamatova.km@gmail.com>
2025-07-27 21:51:25 -0400

    Blue Origin page.tsx placeholder

---
+++ app/contributors/BlueOrigin/page.tsx
@@ -0,0 +1,8 @@
+export default function BlueOriginPage(){
+    return(
+        <div>
+            <h1>ACM Contributions</h1>
+            <p>This page is under construction.</p>
+        </div>
+    );
+}
