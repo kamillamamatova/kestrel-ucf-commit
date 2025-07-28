929ad02844e9f445b01ab828221e683eeacee8e1
kamillamamatova <kamillamamatova.km@gmail.com>
2025-07-27 21:52:28 -0400

    IEEE page.tsx placeholder

---
+++ app/contributors/IEEE/page.tsx
@@ -0,0 +1,8 @@
+export default function IEEEPage(){
+    return(
+        <div>
+            <h1>ACM Contributions</h1>
+            <p>This page is under construction.</p>
+        </div>
+    );
+}
